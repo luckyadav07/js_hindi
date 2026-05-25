d557bd48422e52aa84408c7b990d1488b7928c09
luckyadav07 <luckyyadav61715@gmail.com>
2026-05-25 18:03:17 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountID=14452
+let accountEmail="luckyadav.com"
+var accpass="12345"
+accountcity="delhi"
+
+// accountID=2;
+
+accpass=2313
+accountcity="guu"
+
+
+console.table([accountID,accountEmail,accpass,accountcity])
+
+ 
