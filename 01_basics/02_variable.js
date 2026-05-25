d4b693af1b700ac21c426fc9992130ccfea23747
luckyadav07 <luckyyadav61715@gmail.com>
2026-05-25 18:15:46 +0000

    datatypes

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// alert(3+3) // we are using node js not brwoser
+
+ let name="LUCKY"
+ let age="19"
+
+ // NUM =?2 to power 53
+//  bigint
+// string 
+// boolean
+// null => standalone value
+//undeined
+
+
+// objects
+
+console.log(typeof age);
+
+
