5def4248fc6046ab31b306ff341c748bdf4e86c4
luckyadav07 <luckyyadav61715@gmail.com>
2026-05-26 10:02:00 +0000

    global and local scope

---
+++ 03_basics/02_func.js
@@ -0,0 +1,15 @@
+let a=300;
+const b=10;
+
+if(true){
+    let a=10;
+    const b=338;
+
+    console.log("INNER :", a);
+    console.log("INNER :", b);
+    
+    
+}
+
+console.log(a);
+console.log(b);
