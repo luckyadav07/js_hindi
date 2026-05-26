0b6d38adc2c1b94b87d4e68e47f84fd4600aef6a
luckyadav07 <luckyyadav61715@gmail.com>
2026-05-26 06:54:32 +0000

    mathnum

---
+++ 01_basics/06_nums.js
@@ -0,0 +1,27 @@
+const balance=new Number(100)
+
+console.log(balance);
+
+console.log(balance.toString())
+
+
+
+const othernum=123.28383
+
+console.log(othernum.toPrecision(3))
+
+
+// maths
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+console.log(Math.floor(6.43));
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min) 
+// random gives value between 0 to 1 
+// floor gives closest value
