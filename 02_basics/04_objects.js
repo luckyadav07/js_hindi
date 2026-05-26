20e4e930767982ff5316bdfbd60d76c38469ac42
luckyadav07 <luckyyadav61715@gmail.com>
2026-05-26 08:37:29 +0000

    objects properties

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,59 @@
+ const tinder=new Object()
+
+
+
+ tinder.id="1234"
+ tinder.name="LUCKY"
+ tinder.isloogesin=false
+
+ console.log(tinder);
+
+ const reguser={
+
+    email:"LKYCEN",
+    fullname:{
+        username:{
+            firstname:"LUCKY",
+            lastname:"YADAV"
+        }
+    }
+ }
+
+ console.log(reguser.fullname);
+
+ const obj1={
+    1:"a",
+    2:"b"
+ }
+
+ const obj2={
+    3:"a",
+    4:"b"
+ }
+
+//  const obj3=Object.assign({},obj1,obj2)
+//  console.log(obj3);
+
+ const obj3={...obj1,...obj2} // latest usuage
+ 
+//   console.log(obj3);
+
+const users=[
+    {
+
+    }
+]
+
+console.log(tinder)
+
+console.log(Object.keys(tinder));
+console.log(Object.values(tinder));
+
+console.log(tinder.hasOwnProperty('id'));
+
+
+
+
+
+
+ 
