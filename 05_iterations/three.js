ab5dd886d5acdb8537822a1acdbc08ebfc7b0011
luckyadav07 <luckyyadav61715@gmail.com>
2026-05-27 09:31:46 +0000

    Removed old folders

---
+++ 05_iterations/three.js
@@ -0,0 +1,18 @@
+const arr=[1,2,3,4,5]
+
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const map=new Map()
+map.set('IN','INDIA')
+map.set('USA','USRE')
+
+for (const [key,value] of map) {
+
+    console.log(key, ';-' , value);
+    
+    
+}
